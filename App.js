a88a40bfdefa27fc1813a7b44219cf4991028576
gustavovni08 <gustavovni08@gmail.com>
2023-04-13 12:09:10 -0300

    chupa bruno bessa

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './components/home';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2AAA8A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
